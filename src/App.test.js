c20c2f83c8bffb42f80745483f3c2bfa4ac94bd1
anaoktaa <ana.oktaviana.ana@gmail.com>
2020-12-22 14:41:34 +0700

    adding more configuration for jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import App from './App';
 
 it('expect to render app component', () => {
     expect(shallow(<App/>)).toMatchSnapshot();
 });
+
+
